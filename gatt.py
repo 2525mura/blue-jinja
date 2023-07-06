11eaf8c61ef1c2426c29dcba56276dadb3b9c4bc
Mura <2525mura@gmail.com>
2023-07-06 21:11:59 +0900

    Add file output function

---
+++ gatt.py
@@ -0,0 +1,50 @@
+# GATT Specific setting file
+
+service = {
+    'name': 'Expose',
+    'uuid': '121e8e18-23c1-0bd5-b6d9-6180dba956bc'
+}
+
+characteristics = [
+    {
+        'name': 'Event',
+        'uuid': '1c8e7830-dc60-b4d3-3763-604b1403950a',
+        'direction': 'BIDI',
+        'args': {
+            'type': 'std::string',
+            'size': None,
+            'names': ['msg']
+        }
+    },
+    {
+        'name': 'Lux',
+        'uuid': '16cf81e3-0212-58b9-0380-0dbc6b54c51d',
+        'direction': 'PtoC',
+        'args': {
+            'type': 'float',
+            'size': 4,
+            'names': ['iso', 'f', 'ss', 'lv', 'ev', 'lux']
+        }
+    },
+    {
+        'name': 'RGB',
+        'uuid': '67f46ec5-3d54-54c2-ae2d-fb318a4973b0',
+        'direction': 'PtoC',
+        'args': {
+            'type': 'float',
+            'size': 4,
+            'names': ['r', 'g', 'b', 'ir']
+        }
+    },
+    {
+        'name': 'ISO',
+        'uuid': '241abff2-5d09-b5a3-4a77-cfc19cfac587',
+        'direction': 'CtoP',
+        'args': {
+            'type': 'int',
+            'size': 4,
+            'names': ['iso']
+        }
+    },
+
+]
--- main.py
+++ main.py
@@ -1,63 +1,30 @@
+import os
+import gatt
 from jinja2 import Template, Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.'))
+
+
+def peripheral_esp32_server(path: str):
+    os.makedirs(path, exist_ok=True)
+    with open(f'{path}/BleService{gatt.service["name"]}.h', mode='w') as f:
+        template = env.get_template('template/peripheral/esp32_server/BleService.h.jinja')
+        rendered = template.render(service_conf=gatt.service, characteristics_conf=gatt.characteristics)
+        f.write(rendered)
+
+    with open(f'{path}/BleService{gatt.service["name"]}.cpp', mode='w') as f:
+        template = env.get_template('template/peripheral/esp32_server/BleService.cpp.jinja')
+        rendered = template.render(service_conf=gatt.service, characteristics_conf=gatt.characteristics)
+        f.write(rendered)
+
+    with open(f'{path}/BleService{gatt.service["name"]}Delegate.h', mode='w') as f:
+        template = env.get_template('template/peripheral/esp32_server/BleServiceDelegate.h.jinja')
+        rendered = template.render(service_conf=gatt.service, characteristics_conf=gatt.characteristics)
+        f.write(rendered)
 
 
 def main():
-    env = Environment(loader=FileSystemLoader('.'))
-    template = env.get_template('template/peripheral/esp32/BleService.h.jinja')
-    # template = env.get_template('template/peripheral/esp32/BleService.cpp.jinja')
-    # template = env.get_template('template/peripheral/esp32/BleServiceDelegate.h.jinja')
-
-    service_conf = {
-        'name': 'Expose',
-        'uuid': '4fafc201-1fb5-459e-8fcc-c5c9c331914b'
-    }
-
-    characteristics_conf = [
-        {
-            'name': 'Event',
-            'uuid': 'beb5483e-36e1-4688-b7f5-ea07361b26a8',
-            'direction': 'BIDI',
-            'args': {
-                'type': 'std::string',
-                'size': None,
-                'names': ['msg']
-            }
-        },
-        {
-            'name': 'Lux',
-            'uuid': '16cf81e3-0212-58b9-0380-0dbc6b54c51d',
-            'direction': 'PtoC',
-            'args': {
-                'type': 'float',
-                'size': 4,
-                'names': ['iso', 'f', 'ss', 'lv', 'ev', 'lux']
-            }
-        },
-        {
-            'name': 'RGB',
-            'uuid': '67f46ec5-3d54-54c2-ae2d-fb318a4973b0',
-            'direction': 'PtoC',
-            'args': {
-                'type': 'float',
-                'size': 4,
-                'names': ['r', 'g', 'b', 'ir']
-            }
-        },
-        {
-            'name': 'ISO',
-            'uuid': '241abff2-5d09-b5a3-4a77-cfc19cfac587',
-            'direction': 'CtoP',
-            'args': {
-                'type': 'int',
-                'size': 4,
-                'names': ['iso']
-            }
-        },
-
-    ]
-
-    rendered = template.render(service_conf=service_conf, characteristics_conf=characteristics_conf)
-    print(str(rendered))
+    peripheral_esp32_server(path='out_esp32')
 
 
 if __name__ == '__main__':
